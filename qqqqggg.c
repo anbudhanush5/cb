991a29e460bbb98144077edf36bfe2a2b163d119
anbudhanush5 <35687834+anbudhanush5@users.noreply.github.com>
2018-02-12 13:36:53 +0530

    Create qqqqggg.c

---
+++ qqqqggg.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+include<conio.h>
+void main()
+{
+int a[i],i,j,n,t;
+printf("enter the number of elements");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",array[i]);}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(array[j]<array[i])
+{
+t=array[i];
+array[i]=array[j];
+array[j]=t;
+}
+for(i=o;i<n;i++)
+{
+printf("sored list is %d",array[i]);
+}
+getch();
+}
