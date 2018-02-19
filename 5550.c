f101eeb17077a044deafad11b10c175bcdba70f1
anbudhanush5 <35687834+anbudhanush5@users.noreply.github.com>
2018-02-19 13:31:47 +0530

    Create 5550.c

---
+++ 5550.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int count_words=0,i;
+ int count_char=0;
+ char str[20];
+ printf("Enter string : ");
+scanf("%s",&str);
+ for(i=0; str[i]!=NULL; i++)
+ {
+   count_char++;
+   if(str[i]==' ')
+      count_words++;
+  }
+ printf("\nNumber of words in string : % d",count_words+1); 
+ return 0;
+}
